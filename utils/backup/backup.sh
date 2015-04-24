a29d008c24fcf4a92930f52fbda19700fc0ae25c
Andres Rojas <andres@conpat.io>
2015-04-23 23:31:56 -0400

    Do not archive restore backups

--- utils/backup/backup.sh
+++ utils/backup/backup.sh
@@ -2,10 +2,19 @@
 
 FILENAME="ghost-backup-$(date +%Y-%m-%d)"
 
-cd /var/lib
-tar --verbose --create --file "/backup/${FILENAME}.tar" ghost
+echo "Move restore backups out of the way..."
 
+mv --verbose /var/lib/ghost/.bak /backup/.bak
 echo
 
+echo "Create backup archive..."
+cd /var/lib
+tar --verbose --create --file "/backup/${FILENAME}.tar" ghost
+echo
 cd /backup
 xz --verbose -6e ${FILENAME}.tar
+echo
+
+echo "Put back the restore backups..."
+mv --verbose /backup/.bak /var/lib/ghost/.bak
+echo
